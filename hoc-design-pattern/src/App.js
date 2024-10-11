d119839f7b98cf9d806ae0ec96dcd05cc0db3ac8
Erfan Pournasiri <erfanpournasiri71@gmail.com>
2024-10-11 17:43:14 +0330

    hoc basic example

--- hoc-design-pattern/src/App.js
+++ hoc-design-pattern/src/App.js
@@ -1,9 +1,12 @@
+import { logProps } from "./components/check-props";
+import {UserInfo} from "./components/user-info";
 
+const UserInfoWrapper = logProps(UserInfo)
 
 function App() {
   return (
     <>
-      
+      <UserInfoWrapper test={'test'} message={'hoc design pattern'} part={2}/>
     </>
   );
 }
