79a1b9a3baea0589eead4465967b42b82ca1685f
Erfan Pournasiri <erfanpournasiri71@gmail.com>
2024-10-11 18:06:47 +0330

    data fetching with hoc design pattern

--- hoc-design-pattern/src/App.js
+++ hoc-design-pattern/src/App.js
@@ -1,14 +1,33 @@
-import { logProps } from "./components/check-props";
-import {UserInfo} from "./components/user-info";
 
-const UserInfoWrapper = logProps(UserInfo)
+//for basic example
+// import { logProps } from "./components/check-props";
+// import {UserInfo} from "./components/user-info";
+
+
+// const UserInfoWrapper = logProps(UserInfo)
+
+// function App() {
+  //   return (
+//     <>
+//       <UserInfoWrapper test={'test'} message={'hoc design pattern'} part={2}/>
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+import {UserInfo} from "./components/user-info";
+import { hocFetchingData, } from "./components/hoc-fetching-data";
+
+
+const UserInfoWithUser = hocFetchingData(UserInfo, 2)
 
 function App() {
   return (
     <>
-      <UserInfoWrapper test={'test'} message={'hoc design pattern'} part={2}/>
+      <UserInfoWithUser/>
     </>
   );
 }
-
 export default App;
