ef9df0581c3da9b9f6e35241641834252ff432c7
Erfan Pournasiri <erfanpournasiri71@gmail.com>
2024-10-26 16:28:14 +0330

    another way to provent component re-render

--- re-render-explain-new-issue/src/App.tsx
+++ re-render-explain-new-issue/src/App.tsx
@@ -1,17 +1,28 @@
-import { SlowComponent } from "./components/slow-component";
+import { SlowComponent } from './components/slow-component';
 import {
   AdditionalComplexThings,
   BlaBla,
-  ScrollableContainer,
-} from "./components/dummy-components";
+} from './components/dummy-components';
+import DynamicScroll from './components/dynamic-scroll';
+
+
+//! if we do set state in app component this case our icon that work with state update worked slowly 
+
+//* but when we use dynamic scroll component and then pass outher component as props our component that work with state will work without any problem
+
+
+//* in this way these components are not actually declared or defined here, they are declared and defined in parrent component and they are only accessed with refrence to them 
 
 export default function App() {
   return (
-    <ScrollableContainer>
-      {/* a dynamic block */}
-      <SlowComponent />
-      <BlaBla />
-      <AdditionalComplexThings />
-    </ScrollableContainer>
+    <DynamicScroll
+      content={
+        <>
+          <SlowComponent />
+          <BlaBla />
+          <AdditionalComplexThings />
+        </>
+      }
+    />
   );
 }
